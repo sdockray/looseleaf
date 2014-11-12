47b8a30886f1f0bfa7ea5c0dd6be1ae7a4f3e609
sdockray <sean.patrick.dockray@gmail.com>
2014-11-12 14:36:42 +1100

    dynamic images, added clips and compiled pdfs

--- pdfserver.py
+++ pdfserver.py
@@ -1,5 +1,9 @@
 import mimetypes
 import os
+import re
+import itertools
+import hashlib
+import json
 
 from twisted.web.static import File
 from twisted.web.resource import Resource, NoResource
@@ -31,10 +35,21 @@ class Pdf:
                '-scale-to-x', '-1',
                '-cropbox', '-f', str((start or 0) + 1), '-l', str(end or self.npages), self.path, os.path.join(outdir, 'p')])
         return sorted(glob.glob(os.path.join(outdir, '*')))
+    def dump_to_width(self, w=50, start=None, end=None, outdir=None, codec="jpeg"):
+        outdir = outdir or tempfile.mkdtemp()
+        subprocess.call(['pdftocairo', '-%s' % (codec),
+               '-scale-to-y', '-1',
+               '-scale-to-x', str(w),
+               '-cropbox', '-f', str((start or 0) + 1), '-l', str(end or self.npages), self.path, os.path.join(outdir, 'p')])
+        return sorted(glob.glob(os.path.join(outdir, '*')))
+    def dump_to_pdf(self, start=None, end=None, outdir=None):
+        outdir = outdir or tempfile.mkdtemp()  
+        subprocess.call(['pdfseparate', '-f', str((start or 0) + 1), '-l', str((end or start) + 1), self.path, os.path.join(outdir, 'f_%d.pdf')])
+        return sorted(glob.glob(os.path.join(outdir, '*')))
 
 class Insist(Resource):
     isLeaf = True
-
+    
     def __init__(self, cacheloc):
         self.cacheloc = cacheloc
         self.cachefile = None
@@ -113,6 +128,33 @@ def np2image(np, path):
         mode, (np.shape[1], np.shape[0]), np.tostring())
     im.save(path)    
 
+
+class PdfClip(Insist):
+    def __init__(self, pdf, start, end, cachedir, W=500):
+        self.W = W
+        self.pdf = pdf
+        self.start = start
+        self.end = end
+        Insist.__init__(self, os.path.join(cachedir, "%f-%fx%d.jpg" % (start, end, W)))
+
+    def serialize_computation(self, outpath):
+        # Add new to bottom of base
+        def concat(base, new):
+            return new if base is None else np.concatenate((base,new), axis=0)
+
+        out = None
+        for idx, fpath in enumerate(self.pdf.dump_to_width(w=self.W, codec="png", start=int(self.start), end=int(self.end)+1)):
+            arr = image2np(fpath)
+            top = 0 if out is not None else arr.shape[1] * (self.start - int(self.start))
+            bot = arr.shape[1] if not idx == int(self.end) - int(self.start) else arr.shape[1] * (self.end - int(self.end))
+            if bot > top and arr.shape[1] > bot:
+                arr = arr[:bot]
+            if top > 0:
+                arr = arr[top:]
+            out = concat(out, arr)
+
+        np2image(out, outpath)
+
 class PdfPage(Insist):
     def __init__(self, H, pdf, pageno, cachedir):
         self.H = H
@@ -125,12 +167,14 @@ class PdfPage(Insist):
         os.rename(fname, outpath)
 
 class PdfMosaic(Insist):
-    def __init__(self, pdf, cachedir, W=50, H=72, N_COLS=20):
+    def __init__(self, pdf, cachedir, W=50, H=72, N_COLS=20, start=None, end=None):
         self.W = W
         self.H = H
         self.N_COLS = N_COLS
         self.pdf = pdf
-        Insist.__init__(self, os.path.join(cachedir, "%dx%d.jpg" % (W,H)))
+        self.start = start
+        self.end = end
+        Insist.__init__(self, os.path.join(cachedir, "%dx%dx%d.jpg" % (W,H,N_COLS)))
 
     def serialize_computation(self, outpath):
         # Blank output image
@@ -138,7 +182,7 @@ class PdfMosaic(Insist):
                              min(self.N_COLS, self.pdf.npages) * self.W,
                              3), dtype=np.uint8)
 
-        for idx, fpath in enumerate(self.pdf.dump_to_height(h=self.H, codec="png")):
+        for idx, fpath in enumerate(self.pdf.dump_to_height(h=self.H, codec="png", start=self.start, end=self.end)):
             arr = image2np(fpath)
             if arr.shape[1] > self.W:
                 # Crop horizontally
@@ -157,6 +201,7 @@ class PdfMosaic(Insist):
 
         np2image(out, outpath)
 
+
 class PdfDerivatives(File):
     def __init__(self, pdfpath, cacheroot):
         self.pdf = Pdf(pdfpath)
@@ -165,18 +210,136 @@ class PdfDerivatives(File):
             os.makedirs(cacheroot)
 
         File.__init__(self, pdfpath)
+    
 
-        # Create lazy resources for derivatives
-        mos_page = PdfMosaic(self.pdf, self.cacheroot)
-        gpu_mos_page = PdfMosaic(self.pdf, self.cacheroot, W=128, H=128, N_COLS=32)
-        self.putChild("50x72.jpg", mos_page)
-        self.putChild("128x128.jpg", gpu_mos_page)
-        for h in [128, 1024, 2048]:
-            for p_no in range(self.pdf.npages):
-                pp = PdfPage(h, self.pdf, p_no, self.cacheroot)
-                self.putChild("x%d-%d.jpg" % (h, p_no), pp)
-                # For compatibility, though does not reflect size properly (ala imagemagick geometry)...
-                self.putChild("%dx-%d.jpg" % (h, p_no), pp)                
+    def getChild(self, name, request):
+        # Fall back to serve the PDF if no matches are found
+        c = self
+        # mosaic: wxhxN_COLS, N_COLS optional
+        mosaic_m = re.match('(\d+)x(\d+)(x(\d+))?\.jpg', name)
+        if mosaic_m:
+            w,h,x,cols = mosaic_m.groups()
+            n_cols = 20 if not cols else int(cols)
+            c = PdfMosaic(self.pdf, self.cacheroot, W=int(w), H=int(h), N_COLS=n_cols)
+        # page: xh-p
+        page_m = re.match('x(\d+)-(\d+)\.jpg', name)
+        if page_m:
+            h, p_no = page_m.groups()
+            c = PdfPage(int(h), self.pdf, int(p_no), self.cacheroot)
+        # clip: top-botxh, h optional
+        clip_m = re.match('(\d+(\.\d+)?)-(\d+(\.\d+)?)(x(\d+))?\.jpg', name)
+        if clip_m:
+            top, x, bot, xx, xxx, h = clip_m.groups()
+            c = PdfClip(self.pdf, float(top), float(bot), self.cacheroot)
+        
+        self.putChild(name, c)
+        return c 
+
+
+class PdfCompilation(Resource):
+    def __init__(self, pdfroot, cacheroot):
+        self.pdfroot = pdfroot
+        self.cacheroot = cacheroot
+        self.cachedir = None
+        self.pdf = None
+        Resource.__init__(self)
+
+        self.clear()
+
+    def clear(self):
+        self.contents = []
+        self.hash = None
+
+    def bind(self):
+        # converts the list of all pages to page ranges
+        def ranges(i):
+            for a, b in itertools.groupby(enumerate(i), lambda (x, y): y - x):
+                b = list(b)
+                yield b[0][1], b[-1][1]
+        # check cache
+        pdfpath = os.path.join(self.cachedir, 'pdf.pdf')
+        if not os.path.exists(pdfpath):
+            # build a new pdf from contents
+            parts = []
+            for f, p in self.contents:
+                pdf = Pdf(os.path.join(self.pdfroot, f))
+                p_ranges = ranges(p)
+                for start, end in p_ranges:
+                    parts.extend(pdf.dump_to_pdf(start=start, end=end))
+            cmd = ['pdfunite', pdfpath]
+            cmd[1:1] = parts
+            subprocess.call(cmd)
+        self.pdf = Pdf(pdfpath)
+
+    def compile(self):
+        # hash contents for caching pdf and static files 
+        self.hash = hashlib.md5(str(self.contents)).hexdigest()
+        self.cachedir = os.path.join(self.cacheroot, self.hash)
+        if not os.path.exists(self.cachedir):
+            os.makedirs(self.cachedir)
+
+    def add_child(self, name):
+        # converts a string of comma separated pages, or ranges of pages, or both to an array of pages
+        def pp(s):
+            return sum(((list(range(*[int(j) + k for k,j in enumerate(i.split('-'))])) if '-' in i else [int(i)]) for i in s.split(',')), [])
+        # is the string a valid filename?
+        def ifn(s):
+            return True if os.path.exists(os.path.join(self.pdfroot, name)) else False
+        # is the string a valid page range
+        def ipr(s):
+            return True # @todo - confirm valid pages pattern, eg "2,11-16,25,28,30-32"
+
+        # reset if there are compiled contents already
+        if self.hash is not None:
+            self.clear()
+        
+        is_finalized = False if (self.contents and self.contents[-1][1] is None) else True
+
+        # Add string to the end of contents (either filename or page range)
+        if ifn(name):
+            if is_finalized:
+                self.contents.append((name,None))
+            else:
+                self.contents[-1] = (name, None)
+        elif not is_finalized and ipr(name):
+            self.contents[-1] = (self.contents[-1][0], pp(name))
+
+
+    def getChild(self, name, request):
+        # Try all possible patterns
+        # mosaic: wxhxN_COLS, N_COLS optional
+        mosaic_m = re.match('(\d+)x(\d+)(x(\d+))?\.jpg', name)
+        # page: xh-p
+        page_m = re.match('x(\d+)-(\d+)\.jpg', name)
+        # clip: top-botxh, h optional
+        clip_m = re.match('(\d+(\.\d+)?)-(\d+(\.\d+)?)(x(\d+))?\.jpg', name)
+        # deliver pdf: pdf
+        send_pdf = True if name=='pdf.pdf' else False
+        # Still compiling the contents...
+        if not mosaic_m and not page_m and not clip_m and not send_pdf:
+            self.add_child(name)
+            return self
+        # Finalize the new compiled PDF
+        else:
+            self.compile()
+            self.bind()
+            if not self.pdf:
+                return NoResource()
+            if mosaic_m:
+                w,h,x,cols = mosaic_m.groups()
+                n_cols = 20 if not cols else int(cols)
+                c = PdfMosaic(self.pdf, self.cachedir, W=int(w), H=int(h), N_COLS=n_cols)
+            elif page_m:
+                h, p_no = page_m.groups()
+                c = PdfPage(int(h), self.pdf, int(p_no), self.cachedir)
+            elif clip_m:
+                top, x, bot, xx, xxx, h = clip_m.groups()
+                c = PdfClip(self.pdf, float(top), float(bot), self.cachedir)
+            elif send_pdf:
+                c = File(self.pdf.path)
+        
+        #self.putChild(name, c)
+        return c 
 
 class PdfDirectory(Resource):
     def __init__(self, pdfdir, cachedir):
@@ -184,6 +347,9 @@ class PdfDirectory(Resource):
         self.cachedir = cachedir
 
         Resource.__init__(self)
+
+        c = PdfCompilation(self.pdfdir, self.cachedir)
+        self.putChild('compile', c)
 
         # Find all children up-front
         # Commented out--we will add children as needed
@@ -193,7 +359,7 @@ class PdfDirectory(Resource):
     def add_child(self, path):
         if os.path.isdir(path):
             c = PdfDirectory(os.path.join(self.pdfdir, path), os.path.join(self.cachedir, path))
-        elif path.endswith(".pdf"):
+        elif path.lower().endswith(".pdf"):
             c = PdfDerivatives(os.path.join(self.pdfdir, path), os.path.join(self.cachedir, path))
         else:
             c = File(os.path.join(self.pdfdir, path))
@@ -203,6 +369,7 @@ class PdfDirectory(Resource):
 
     def getChild(self, name, request):
         out = Resource.getChild(self, name, request)
+
         if name and out.code == 404 and os.path.exists(os.path.join(self.pdfdir, name)):
             c = self.add_child(name)
             return c
